b10954f766bd5526a7e15749db181cba433ebd34
yingzi <yingzi@example.com>
2019-10-23 10:50:50 +0800

    build

---
+++ build/precache-manifest.f44104762f943b0e0ad85165f0ad6af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701157ad4c4ea4e564c8647f2acb29cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e820f4e0a7d1d2c4b340",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "59c5a9ef632d3a1056b7",
+    "url": "/static/css/main.9512b973.chunk.css"
+  },
+  {
+    "revision": "e820f4e0a7d1d2c4b340",
+    "url": "/static/js/2.6dacd6ef.chunk.js"
+  },
+  {
+    "revision": "59c5a9ef632d3a1056b7",
+    "url": "/static/js/main.cdb36bdd.chunk.js"
+  },
+  {
+    "revision": "12b3adbaa2aefd3ebbfd",
+    "url": "/static/js/runtime-main.c9eace70.js"
+  }
+]);
